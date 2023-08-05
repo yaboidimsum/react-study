602d87b4e910b1fffb796e50cc087761b8ea93b6
dimsum <101172637+yaboidimsum@users.noreply.github.com>
2023-08-05 23:06:35 +0700

    lanjut react

--- react-mosh/react-app/src/App.tsx
+++ react-mosh/react-app/src/App.tsx
@@ -1,9 +1,25 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = [
+    "Nueva York",
+    "Los Angeles",
+    "Chicago",
+    "Phoenix",
+    "Philadelphia",
+  ];
+
+  let people = [
+    "John", "Mary", "Peter"];
+
+const handleSelectItem = (item: string) => {
+  console.log(item);
+};
+
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
+      <ListGroup items={people} heading="Name" onSelectItem={handleSelectItem} />
     </div>
   );
 }
--- react-mosh/react-app/src/components/ListGroup.tsx
+++ react-mosh/react-app/src/components/ListGroup.tsx
@@ -1,18 +1,41 @@
-function ListGroup() {
-  const items = [
-    "Nueva York",
-    "Los Angeles",
-    "Chicago",
-    "Phoenix",
-    "Philadelphia",
-  ];
+import { MouseEvent, useState } from "react";
+
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  //Event Handler
+  // const handleClick = (e: MouseEvent) => {
+  //   console.log(e);
+  // };
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
+      {/* Best Practices */}
+      {/* Using e for event */}
+      {items.length === 0 && <h1>No List Found</h1>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
