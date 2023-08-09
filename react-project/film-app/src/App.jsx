92b55b249e5f6a225ab19b1c1abefcfd7dc42356
dimsum <101172637+yaboidimsum@users.noreply.github.com>
2023-08-09 23:10:47 +0700

    add react project

--- react-project/film-app/src/App.jsx
+++ react-project/film-app/src/App.jsx
@@ -1,24 +1,67 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
+import SearchIcon from "./search.svg";
 
-const Name = (props) => {
-  return <h1>My name is: {props.name}</h1>;
+//1e6c1228
+
+const API_URL = "http://omdbapi.com?apikey=1e6c1228";
+
+const movie1 = {
+  Title: "Star Wars: Episode VII - The Force Awakens",
+  Year: "2015",
+  imdbID: "tt2488496",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg",
 };
 
 const App = () => {
-  const name = "React App";
-  const isNameShowing = true;
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Star Wars");
+  }, []);
+
   return (
-    <>
-      <div className="App">
-        <h1>Hello {24 + 45}</h1>
-        {isNameShowing ? <h2>{name}</h2> : <h1>There is no name</h1>}
+    <div className="app">
+      <h1>BrownyMoviey</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => {}}></img>
       </div>
-      <Name name="Test" />
-      <Name name="Test1" />
-      <Name name="Test2" />
-      <Name name="Test3" />
-    </>
+      <div>
+        <div className="container">
+          <div className="movie">
+            <div>
+              <p>{movie1.Year}</p>
+            </div>
+            <div>
+              <img
+                src={
+                  movie1.Poster !== "N/A"
+                    ? movie1.Poster
+                    : "https://via.placeholder.com/400"
+                }
+                alt={movie1.Title}
+              />
+            </div>
+            <div>
+              <span>{movie1.Type}</span>
+              <h3>{movie1.Title}</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
